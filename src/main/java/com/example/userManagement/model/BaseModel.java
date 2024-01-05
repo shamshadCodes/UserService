3e95bcef643c9f0c19e24b0b7af98215907736d4
shamshad <lookatitonce@gmail.com>
2024-01-05 21:22:59 +0530

    removing unneccsary @GenericGenerator attribute

--- src/main/java/com/example/userManagement/model/BaseModel.java
+++ src/main/java/com/example/userManagement/model/BaseModel.java
@@ -7,8 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 
 import java.util.UUID;
 
@@ -17,8 +15,7 @@ import java.util.UUID;
 @MappedSuperclass
 public class BaseModel {
     @Id
-    @GeneratedValue(generator = "uuidGenerator")
-    @GenericGenerator(name = "uuidGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid2")
     @Column(columnDefinition = "UUID", nullable = false, updatable = false)
     private UUID id;
 }
