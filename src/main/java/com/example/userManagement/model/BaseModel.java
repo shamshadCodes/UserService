a4d23833dcdaf0cd44cfe25fa45a29f6eb5802b1
shamshad <lookatitonce@gmail.com>
2024-01-05 21:19:02 +0530

    fixed error with UUID and ambiguity on 'User' entity name

--- src/main/java/com/example/userManagement/model/BaseModel.java
+++ src/main/java/com/example/userManagement/model/BaseModel.java
@@ -8,6 +8,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import java.util.UUID;
 
@@ -17,7 +18,7 @@ import java.util.UUID;
 public class BaseModel {
     @Id
     @GeneratedValue(generator = "uuidGenerator")
-    @GenericGenerator(name = "uuidGenerator", strategy = "uuid")
-    @Column(columnDefinition = "CHAR(36)", nullable = false, updatable = false)
+    @GenericGenerator(name = "uuidGenerator", strategy = "uuid2")
+    @Column(columnDefinition = "UUID", nullable = false, updatable = false)
     private UUID id;
 }
--- src/main/java/com/example/userManagement/model/User.java
+++ src/main/java/com/example/userManagement/model/User.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "Auth_User")
 public class User extends BaseModel {
     private String name;
     private String email;
