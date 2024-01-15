95a8a50e15e8d0d112340b5f1f85eda4c43b509b
shamshad <lookatitonce@gmail.com>
2024-01-15 18:56:03 +0530

    Security config file added

--- src/main/java/com/example/UserManagementService/security/CustomSpringUserDetails.java
+++ src/main/java/com/example/UserManagementService/security/CustomSpringUserDetails.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 
 public class CustomSpringUserDetails implements UserDetails {
-    private User user;
+    private final User user;
 
     public CustomSpringUserDetails(User user) {
         this.user = user;
